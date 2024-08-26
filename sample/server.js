363a7ad13435a684d359613040f3dfdd3ba7807b
mporto <martinporto@gmail.com>
2024-08-26 17:45:34 -0300

    practica de dependencias

---
+++ sample/server.js
@@ -0,0 +1,5 @@
+import add from '@martinporto/small-node-lib';
+import addLocal from './tools.js';
+
+console.log(add(2, 3));
+console.log(addLocal(1, 2));
---
+++ sample/tools.js
@@ -0,0 +1,5 @@
+function addLocal(a, b) {
+  return a + b;
+}
+
+export default addLocal;
